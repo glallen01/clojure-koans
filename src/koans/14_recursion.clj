64995241d411d0fe0175c391e3ac670ec839a441
George Allen <george@simspace.com>
2017-01-20 06:13:18 -0500

    completed 14,15

--- src/koans/14_recursion.clj
+++ src/koans/14_recursion.clj
@@ -3,21 +3,30 @@
 
 (defn is-even? [n]
   (if (= n 0)
-    __
-    (___ (is-even? (dec n)))))
+    true
+    (not (is-even? (dec n)))))
 
 (defn is-even-bigint? [n]
   (loop [n   n
          acc true]
     (if (= n 0)
-      __
+      acc
       (recur (dec n) (not acc)))))
 
 (defn recursive-reverse [coll]
-  __)
+  (loop [coll coll
+         acc '()]
+    (if (empty? coll)
+      acc
+      (recur (rest coll)(cons (first coll) acc)))))
 
 (defn factorial [n]
-  __)
+  (loop [n n
+         acc n]
+    (if (= 1 n)
+      acc
+      (recur (- n 1)
+             (* acc (- n 1))))))
 
 (meditations
   "Recursion ends with a base case"
